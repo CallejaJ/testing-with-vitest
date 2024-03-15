471d789e06dca92291efa04a1fd9a83b0601dfa8
Jorge Calleja <posicionadoenlaweb@gmail.com>
2024-03-15 23:22:42 +0100

    access to the matchers

--- setUpTest.js
+++ setUpTest.js
@@ -2,6 +2,9 @@ import { expect, afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
 
+/* Giving access to the matchers 
+It will be automatically executed before running your tests */
+
 expect.extend(matchers);
 
 afterEach(() => {
